46aa48967f7acbf282f7d7408f07bd8a8416c6b8
paulo-madruga <paulo.madruga@gmail.com>
2026-03-02 22:02:06 +0000

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-// Self-destructing service worker — unregisters itself and clears all caches
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
-      .then(() => self.registration.unregister())
-  );
-});
